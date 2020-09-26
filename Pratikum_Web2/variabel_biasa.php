531a4033db78a45f67a5ef04865460ef101e61bd
ajisaputra1996 <71937822+ajisaputra1996@users.noreply.github.com>
2020-09-26 23:12:32 +0700

    Add files via upload

---
+++ Pratikum_Web2/variabel_biasa.php
@@ -0,0 +1,10 @@
+<?php
+$nama = "aji saputra";
+$nim = 2018804503;
+$kelas = "sistem Informasi malam";
+echo "Nama Mahasiswa : $nama";
+echo "<br>";
+echo "Nim Mahasiswa : $nim";
+echo "<br>";
+echo "Kelas : $kelas";
+?>
