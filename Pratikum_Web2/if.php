50b0f2c9d5c609a63d32492c6ed945bb3f607f2d
ajisaputra1996 <71937822+ajisaputra1996@users.noreply.github.com>
2020-10-14 17:38:31 +0700

    Add files via upload

---
+++ Pratikum_Web2/if.php
@@ -0,0 +1,30 @@
+<html>
+<head>
+<title>PENGGUNAAN IF ELSE</title>
+</head>
+<body>
+Nilai A = 2 <br>
+Nilai B = 5 <br>
+Nilai C = 9 <br><br>
+
+
+<br>
+<?php
+$a = 2;
+$b = 5;
+$c = 9;
+if($b > $a)
+{
+echo "A lebih besar dari B";
+}
+else if($a > $c)
+{
+echo "A lebih besar dari C";
+}
+else
+{
+echo "a lebih kecil dari B dan C";
+}
+?>
+</body>
+</html>
